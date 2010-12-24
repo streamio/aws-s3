6c02081518652eb48ca0d9f4c8b487d1ed8cba5f
Santanu Karmakar <santanu@fotomoto.com>
2010-12-24 15:32:55 +0530

    add eu bucket support, taken from isaacfeliu

--- lib/aws/s3/bucket.rb
+++ lib/aws/s3/bucket.rb
@@ -1,19 +1,19 @@
 module AWS
   module S3
     # Buckets are containers for objects (the files you store on S3). To create a new bucket you just specify its name.
-    # 
-    #   # Pick a unique name, or else you'll get an error 
+    #
+    #   # Pick a unique name, or else you'll get an error
     #   # if the name is already taken.
     #   Bucket.create('jukebox')
-    # 
+    #
     # Bucket names must be unique across the entire S3 system, sort of like domain names across the internet. If you try
     # to create a bucket with a name that is already taken, you will get an error.
     #
     # Assuming the name you chose isn't already taken, your new bucket will now appear in the bucket list:
-    # 
+    #
     #   Service.buckets
     #   # => [#<AWS::S3::Bucket @attributes={"name"=>"jukebox"}>]
-    # 
+    #
     # Once you have succesfully created a bucket you can you can fetch it by name using Bucket.find.
     #
     #   music_bucket = Bucket.find('jukebox')
@@ -33,20 +33,20 @@ module AWS
     # be found in the documentation for Bucket.find.
     #
     # To add an object to a bucket you specify the name of the object, its value, and the bucket to put it in.
-    # 
+    #
     #   file = 'black-flowers.mp3'
     #   S3Object.store(file, open(file), 'jukebox')
     #
     # You'll see your file has been added to it:
-    # 
+    #
     #   music_bucket.objects
     #   # => [#<AWS::S3::S3Object '/jukebox/black-flowers.mp3'>]
-    # 
+    #
     # You can treat your bucket like a hash and access objects by name:
-    # 
+    #
     #   jukebox['black-flowers.mp3']
     #   # => #<AWS::S3::S3Object '/jukebox/black-flowers.mp3'>
-    # 
+    #
     # In the event that you want to delete a bucket, you can use Bucket.delete.
     #
     #   Bucket.delete('jukebox')
@@ -60,7 +60,7 @@ module AWS
     #   # => true
     class Bucket < Base
       class << self
-        # Creates a bucket named <tt>name</tt>. 
+        # Creates a bucket named <tt>name</tt>.
         #
         #   Bucket.create('jukebox')
         #
@@ -72,18 +72,35 @@ module AWS
         #
         #   Bucket.create('internet_drop_box', :access => :public_read_write)
         #
-        # The full list of access levels that you can set on Bucket and S3Object creation are listed in the README[link:files/README.html] 
+        # The full list of access levels that you can set on Bucket and S3Object creation are listed in the README[link:files/README.html]
         # in the section called 'Setting access levels'.
         def create(name, options = {})
+          self.current_host = name
           validate_name!(name)
-          put("/#{name}", options).success?
+          put("/", options, BucketConfiguration.new(options[:location]).to_s).success?
         end
-        
-        # Fetches the bucket named <tt>name</tt>. 
+
+        class BucketConfiguration < XmlGenerator
+          attr_reader :location
+
+          def initialize(location)
+            @location = location
+            super()
+          end
+
+          def build
+            return nil unless location == :eu
+            xml.tag!('CreateBucketConfiguration') do
+              xml.LocationConstraint 'EU'
+            end
+          end
+        end
+
+        # Fetches the bucket named <tt>name</tt>.
         #
         #   Bucket.find('jukebox')
         #
-        # If a default bucket is inferable from the current connection's subdomain, or if set explicitly with Base.set_current_bucket, 
+        # If a default bucket is inferable from the current connection's subdomain, or if set explicitly with Base.set_current_bucket,
         # it will be used if no bucket is specified.
         #
         #   MusicBucket.current_bucket
@@ -95,21 +112,21 @@ module AWS
         # You can access your objects using the Bucket#objects method.
         #
         #   Bucket.find('jukebox').objects
-        # 
+        #
         # There are several options which allow you to limit which objects are retrieved. The list of object filtering options
         # are listed in the documentation for Bucket.objects.
         def find(name = nil, options = {})
           new(get(path(name, options)).bucket)
         end
-        
+
         # Return just the objects in the bucket named <tt>name</tt>.
         #
         # By default all objects of the named bucket will be returned. There are options, though, for filtering
         # which objects are returned.
         #
         # === Object filtering options
-        # 
-        # * <tt>:max_keys</tt> - The maximum number of keys you'd like to see in the response body. 
+        #
+        # * <tt>:max_keys</tt> - The maximum number of keys you'd like to see in the response body.
         #   The server may return fewer than this many keys, but will not return more.
         #
         #     Bucket.objects('jukebox').size
@@ -124,10 +141,10 @@ module AWS
         #     Bucket.objects('jukebox', :prefix => 'classical')
         #     # => [<AWS::S3::S3Object '/classical/malher.mp3'>]
         #
-        # * <tt>:marker</tt> - Marker specifies where in the result set to resume listing. It restricts the response 
-        #   to only contain results that occur alphabetically _after_ the value of marker. To retrieve the next set of results, 
+        # * <tt>:marker</tt> - Marker specifies where in the result set to resume listing. It restricts the response
+        #   to only contain results that occur alphabetically _after_ the value of marker. To retrieve the next set of results,
         #   use the last key from the current page of results as the marker in your next request.
-        # 
+        #
         #     # Skip 'mahler'
         #     Bucket.objects('jukebox', :marker => 'mb')
         #     # => [<AWS::S3::S3Object '/jazz/miles.mp3'>]
@@ -144,10 +161,10 @@ module AWS
         def objects(name = nil, options = {})
           find(name, options).object_cache
         end
-        
+
         # Deletes the bucket named <tt>name</tt>.
         #
-        # All objects in the bucket must be deleted before the bucket can be deleted. If the bucket is not empty, 
+        # All objects in the bucket must be deleted before the bucket can be deleted. If the bucket is not empty,
         # BucketNotEmpty will be raised.
         #
         # You can side step this issue by passing the :force => true option to delete which will take care of
@@ -158,41 +175,43 @@ module AWS
         # Only the owner of a bucket can delete a bucket, regardless of the bucket's access control policy.
         def delete(name = nil, options = {})
           find(name).delete_all if options[:force]
-          
+
           name = path(name)
           Base.delete(name).success?
         end
-        
+
         # List all your buckets. This is a convenient wrapper around AWS::S3::Service.buckets.
         def list(reload = false)
+          self.current_host = nil
           Service.buckets(reload)
         end
-        
+
         private
           def validate_name!(name)
             raise InvalidBucketName.new(name) unless name =~ /^[-\w.]{3,255}$/
           end
-          
+
           def path(name, options = {})
             if name.is_a?(Hash)
               options = name
               name    = nil
             end
-            "/#{bucket_name(name)}#{RequestOptions.process(options).to_query_string}"
+            self.current_host = bucket_name(name)
+            "/#{RequestOptions.process(options).to_query_string}"
           end
       end
-      
+
       attr_reader :object_cache #:nodoc:
-      
+
       include Enumerable
-      
+
       def initialize(attributes = {}) #:nodoc:
         super
         @object_cache = []
         build_contents!
       end
-      
-      # Fetches the object named <tt>object_key</tt>, or nil if the bucket does not contain an object with the 
+
+      # Fetches the object named <tt>object_key</tt>, or nil if the bucket does not contain an object with the
       # specified key.
       #
       #   bucket.objects
@@ -203,7 +222,7 @@ module AWS
       def [](object_key)
         detect {|file| file.key == object_key.to_s}
       end
-      
+
       # Initializes a new S3Object belonging to the current bucket.
       #
       #   object = bucket.new_object
@@ -217,7 +236,7 @@ module AWS
         register(object)
         object
       end
-      
+
       # List S3Object's of the bucket.
       #
       # Once fetched the objects will be cached. You can reload the objects by passing <tt>:reload</tt>.
@@ -238,11 +257,11 @@ module AWS
           reload  = options
           options = {}
         end
-        
+
         reload!(options) if reload || object_cache.empty?
         object_cache
       end
-      
+
       # Iterates over the objects in the bucket.
       #
       #   bucket.each do |object|
@@ -252,23 +271,23 @@ module AWS
         # Dup the collection since we might be destructively modifying the object_cache during the iteration.
         objects.dup.each(&block)
       end
-      
+
       # Returns true if there are no objects in the bucket.
       def empty?
         objects.empty?
       end
-      
+
       # Returns the number of objects in the bucket.
       def size
         objects.size
       end
-      
+
       # Deletes the bucket. See its class method counter part Bucket.delete for caveats about bucket deletion and how to ensure
       # a bucket is deleted no matter what.
       def delete(options = {})
         self.class.delete(name, options)
       end
-      
+
       # Delete all files in the bucket. Use with caution. Can not be undone.
       def delete_all
         each do |object|
@@ -277,43 +296,43 @@ module AWS
         self
       end
       alias_method :clear, :delete_all
-      
+
       # Buckets observe their objects and have this method called when one of their objects
       # is either stored or deleted.
-      def update(action, object) #:nodoc:        
+      def update(action, object) #:nodoc:
         case action
         when :stored  then add object unless objects.include?(object)
         when :deleted then object_cache.delete(object)
         end
       end
-      
-      private        
+
+      private
         def build_contents!
           return unless has_contents?
           attributes.delete('contents').each do |content|
             add new_object(content)
           end
         end
-        
+
         def has_contents?
           attributes.has_key?('contents')
         end
-        
+
         def add(object)
           register(object)
           object_cache << object
         end
-        
+
         def register(object)
           object.bucket = self
         end
-        
+
         def reload!(options = {})
           object_cache.clear
-          self.class.objects(name, options).each do |object| 
+          self.class.objects(name, options).each do |object|
             add object
           end
-        end         
+        end
     end
   end
 end
--- lib/aws/s3/logging.rb
+++ lib/aws/s3/logging.rb
@@ -1,30 +1,30 @@
 module AWS
   module S3
     # A bucket can be set to log the requests made on it. By default logging is turned off. You can check if a bucket has logging enabled:
-    # 
+    #
     #   Bucket.logging_enabled_for? 'jukebox'
     #   # => false
-    # 
+    #
     # Enabling it is easy:
-    # 
+    #
     #   Bucket.enable_logging_for('jukebox')
-    # 
+    #
     # Unless you specify otherwise, logs will be written to the bucket you want to log. The logs are just like any other object. By default they will start with the prefix 'log-'. You can customize what bucket you want the logs to be delivered to, as well as customize what the log objects' key is prefixed with by setting the <tt>target_bucket</tt> and <tt>target_prefix</tt> option:
-    # 
+    #
     #   Bucket.enable_logging_for(
     #     'jukebox', 'target_bucket' => 'jukebox-logs'
     #   )
-    # 
+    #
     # Now instead of logging right into the jukebox bucket, the logs will go into the bucket called jukebox-logs.
-    # 
+    #
     # Once logs have accumulated, you can access them using the <tt>logs</tt> method:
-    # 
+    #
     #   pp Bucket.logs('jukebox')
     #   [#<AWS::S3::Logging::Log '/jukebox-logs/log-2006-11-14-07-15-24-2061C35880A310A1'>,
     #    #<AWS::S3::Logging::Log '/jukebox-logs/log-2006-11-14-08-15-27-D8EEF536EC09E6B3'>,
     #    #<AWS::S3::Logging::Log '/jukebox-logs/log-2006-11-14-08-15-29-355812B2B15BD789'>]
     #
-    # Each log has a <tt>lines</tt> method that gives you information about each request in that log. All the fields are available 
+    # Each log has a <tt>lines</tt> method that gives you information about each request in that log. All the fields are available
     # as named methods. More information is available in Logging::Log::Line.
     #
     #   logs = Bucket.logs('jukebox')
@@ -45,32 +45,32 @@ module AWS
       # the status object will indicate what bucket the logs are written to via the <tt>target_bucket</tt> method as well as
       # the logging key prefix with via <tt>target_prefix</tt>.
       #
-      # See the documentation for Logging::Management::ClassMethods for more information on how to get the logging status of a bucket. 
+      # See the documentation for Logging::Management::ClassMethods for more information on how to get the logging status of a bucket.
       class Status
         include SelectiveAttributeProxy
         attr_reader :enabled
         alias_method :logging_enabled?, :enabled
-      
+
         def initialize(attributes = {}) #:nodoc:
           attributes  = {'target_bucket' => nil, 'target_prefix' => nil}.merge(attributes)
           @enabled    = attributes.has_key?('logging_enabled')
           @attributes = attributes.delete('logging_enabled') || attributes
         end
-      
+
         def to_xml #:nodoc:
           Builder.new(self).to_s
         end
-      
+
         private
           attr_reader :attributes
-        
+
           class Builder < XmlGenerator #:nodoc:
             attr_reader :logging_status
             def initialize(logging_status)
               @logging_status = logging_status
               super()
             end
-          
+
             def build
               xml.tag!('BucketLoggingStatus', 'xmlns' => 'http://s3.amazonaws.com/doc/2006-03-01/') do
                 if logging_status.target_bucket && logging_status.target_prefix
@@ -83,19 +83,19 @@ module AWS
             end
           end
         end
-      
+
       # A bucket log exposes requests made on the given bucket. Lines of the log represent a single request. The lines of a log
-      # can be accessed with the lines method. 
+      # can be accessed with the lines method.
       #
       #   log = Bucket.logs_for('marcel').first
       #   log.lines
-      # 
+      #
       # More information about the logged requests can be found in the documentation for Log::Line.
       class Log
         def initialize(log_object) #:nodoc:
           @log = log_object
         end
-        
+
         # Returns the lines for the log. Each line is wrapped in a Log::Line.
         if RUBY_VERSION >= '1.8.7'
           def lines
@@ -107,18 +107,18 @@ module AWS
           end
         end
         memoized :lines
-        
+
         def path
           log.path
         end
-        
+
         def inspect #:nodoc:
           "#<%s:0x%s '%s'>" % [self.class.name, object_id, path]
         end
-        
+
         private
           attr_reader :log
-          
+
         # Each line of a log exposes the raw line, but it also has method accessors for all the fields of the logged request.
         #
         # The list of supported log line fields are listed in the S3 documentation: http://docs.amazonwebservices.com/AmazonS3/2006-03-01/LogFormat.html
@@ -139,12 +139,12 @@ module AWS
           QUOTED_STRING = /"([^"]+)"/
           REST          = /(\S+)/
           LINE_SCANNER  = /#{DATE}|#{QUOTED_STRING}|#{REST}/
-          
+
           cattr_accessor :decorators
           @@decorators = Hash.new {|hash, key| hash[key] = lambda {|entry| CoercibleString.coerce(entry)}}
           cattr_reader   :fields
           @@fields     = []
-          
+
           class << self
             def field(name, offset, type = nil, &block) #:nodoc:
               decorators[name] = block if block_given?
@@ -161,20 +161,23 @@ module AWS
                 memoized :#{name}
               EVAL
             end
-            
+
             # Time.parse doesn't like %d/%B/%Y:%H:%M:%S %z so we have to transform it unfortunately
             def typecast_time(datetime) #:nodoc:
               datetime.sub!(%r|^(\w{2})/(\w{3})/(\w{4})|, '\2 \1 \3')
+              if Date.constants.include?('ABBR_MONTHS')
+                datetime.sub!(month, Date::ABBR_MONTHS[month.downcase].to_s)
+              end
               datetime.sub!(':', ' ')
               Time.parse(datetime)
             end
           end
-          
+
           def initialize(line) #:nodoc:
             super(line)
             @parts = parse
           end
-          
+
           field(:owner,            1)  {|entry| Owner.new('id' => entry) }
           field :bucket,           2
           field(:time,             3)  {|entry| typecast_time(entry)}
@@ -192,7 +195,7 @@ module AWS
           field :turn_around_time, 15
           field :referrer,         16
           field :user_agent,       17
-          
+
           # Returns all fields of the line in a hash of the form <tt>:field_name => :field_value</tt>.
           #
           #   line.attributes.values_at(:bucket, :key)
@@ -203,22 +206,22 @@ module AWS
               attribute_hash
             end
           end
-          
+
           private
             attr_reader :parts
-            
+
             def parse
               scan(LINE_SCANNER).flatten.compact
             end
         end
       end
-      
+
       module Management #:nodoc:
         def self.included(klass) #:nodoc:
           klass.extend(ClassMethods)
           klass.extend(LoggingGrants)
         end
-        
+
         module ClassMethods
           # Returns the logging status for the bucket named <tt>name</tt>. From the logging status you can determine the bucket logs are delivered to
           # and what the bucket object's keys are prefixed with. For more information see the Logging::Status class.
@@ -234,7 +237,7 @@ module AWS
             status ? put(path, {}, status.to_xml) : Status.new(get(path).parsed)
           end
           alias_method :logging_status, :logging_status_for
-          
+
           # Enables logging for the bucket named <tt>name</tt>. You can specify what bucket to log to with the <tt>'target_bucket'</tt> option as well
           # as what prefix to add to the log files with the <tt>'target_prefix'</tt> option. Unless you specify otherwise, logs will be delivered to
           # the same bucket that is being logged and will be prefixed with <tt>log-</tt>.
@@ -246,19 +249,19 @@ module AWS
             logging_status(name, Status.new(options))
           end
           alias_method :enable_logging, :enable_logging_for
-          
+
           # Disables logging for the bucket named <tt>name</tt>.
           def disable_logging_for(name = nil)
             logging_status(bucket_name(name), Status.new)
           end
           alias_method :disable_logging, :disable_logging_for
-          
+
           # Returns true if logging has been enabled for the bucket named <tt>name</tt>.
           def logging_enabled_for?(name = nil)
             logging_status(bucket_name(name)).logging_enabled?
           end
           alias_method :logging_enabled?, :logging_enabled_for?
-          
+
           # Returns the collection of logs for the bucket named <tt>name</tt>.
           #
           #   Bucket.logs_for 'marcel'
@@ -269,7 +272,7 @@ module AWS
               options = name
               name    = nil
             end
-            
+
             name           = bucket_name(name)
             logging_status = logging_status_for(name)
             return [] unless logging_status.logging_enabled?
@@ -279,7 +282,7 @@ module AWS
           end
           alias_method :logs, :logs_for
         end
-        
+
         module LoggingGrants #:nodoc:
           def grant_logging_access_to_target_bucket(target_bucket)
             acl = acl(target_bucket)
@@ -288,23 +291,23 @@ module AWS
             acl(target_bucket, acl)
           end
         end
-        
+
         def logging_status
           self.class.logging_status_for(name)
         end
-        
+
         def enable_logging(*args)
           self.class.enable_logging_for(name, *args)
         end
-        
+
         def disable_logging(*args)
           self.class.disable_logging_for(name, *args)
         end
-        
+
         def logging_enabled?
           self.class.logging_enabled_for?(name)
         end
-        
+
         def logs(options = {})
           self.class.logs_for(name, options)
         end
